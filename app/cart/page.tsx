d2b1c1815056efc925e2e8e2a1df6c88af75bd50
Daniel Rojas <notdanielrojas@gmail.com>
2024-10-07 20:17:51 -0300

    cart translated to eng

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -28,17 +28,17 @@ export default function Cart() {
 
   return (
     <div className={styles.cartSection}>
-      <h2>Carrito de Compras</h2>
+      <h2>Shopping Cart</h2>
       {savedCart.length === 0 ? (
-        <p>Tu carrito está vacío</p>
+        <p>Your Cart is empty</p>
       ) : (
         <table className={styles.cartTable}>
           <thead>
             <tr>
-              <th>Producto</th>
-              <th>Imagen</th>
-              <th>Precio</th>
-              <th>Cantidad</th>
+              <th>Product</th>
+              <th>Image</th>
+              <th>Price</th>
+              <th>Quantity</th>
               <th>Total</th>
             </tr>
           </thead>
