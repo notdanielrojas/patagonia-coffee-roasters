c269b6702900ebcc81130b83ef9d38b608d9f709
Daniel Rojas <notdanielrojas@gmail.com>
2024-10-13 19:03:12 -0300

    tables added

--- app/server/tables.sql
+++ app/server/tables.sql
@@ -7,3 +7,28 @@ CREATE TABLE users (id INT )CREATE TABLE users (
     register_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    image_url TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+ 
+
+ CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    order_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE cart_details(
+    id integer NOT NULL ,
+    order_id integer,
+    quantity integer NOT NULL,
+    price numeric(10, 2) NOT NULL,
+    total numeric(10, 2) GENERATED ALWAYS AS (((quantity)::numeric * price)) STORED,
+    CONSTRAINT cart_details_pkey PRIMARY KEY (id),
+    CONSTRAINT cart_details_order_id_fkey FOREIGN KEY (order_id) REFERENCES public.orders (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE
+)
