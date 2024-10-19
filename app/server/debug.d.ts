d97a1cd23cf59483907c130ff87fb1baa91ca4fc
Daniel Rojas <notdanielrojas@gmail.com>
2024-10-19 00:37:38 -0300

    build 4

--- app/server/debug.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "debug" {
-  export function debug(namespace: string): (...args: any[]) => void;
-  export function enable(namespaces: string): void;
-  export function disable(): void;
-  export const formatters: { [key: string]: (v: any) => string };
-}
